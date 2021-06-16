32d9e47d8fe0cb83b4d2f3ce1084e1bf00f846ca
Sam.Tek <sonny.foot@hotmail.com>
2021-06-16 14:34:18 +0200

    Add QuestionBusiness

---
+++ Business/QuestionBusiness.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Repositories.Contracts;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class QuestionBusiness
+    {
+        private IQuestionRepository _questionRepository;
+        
+        public QuestionBusiness(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public async Task<IQueryable<Question>> GetQuestionsAsync()
+        {
+            return await _questionRepository.GetQuestionsAsync();
+        }
+
+        public async Task<Question> DetailAsync(int id)
+        {
+            return await _questionRepository.DetailAsync(id);
+        }
+
+        public async Task CreateAsync(Question question)
+        {
+            await _questionRepository.CreateAsync(question);
+        }
+
+        public async Task EditAsync(Question question)
+        {
+            await _questionRepository.EditAsync(question);
+        }
+
+        public async Task DeleteAsync(Question question)
+        {
+            await _questionRepository.DeleteAsync(question);
+        }
+    }
+}
