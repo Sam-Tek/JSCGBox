4b53ca9e2bd0a429bff72ad976efe715e7520ce9
Sam.Tek <sonny.foot@hotmail.com>
2021-06-16 12:45:07 +0200

    Add UserRepository and a little test to add a user

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,28 @@
-﻿using Repositories;
+﻿using Entities;
+using Repositories;
 using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             using (var context = new JSCGContext())
             {
                 context.Initialize(true);
+
+                UserRepository userRepository = new UserRepository(context);
+
+                //test add user 
+                await userRepository.CreateAsync(new User() 
+                { 
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "John@Doe.fr"
+                });
 
                 foreach (var item in context.Users)
                 {
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UserRepository
+    {
+        private JSCGContext _context;
+
+        public UserRepository(JSCGContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IQueryable<User>> GetUsersAsync()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users.AsQueryable();
+        }
+
+        public async Task<User> DetailsAsync(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task EditAsync(User user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(User user)
+        {
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
