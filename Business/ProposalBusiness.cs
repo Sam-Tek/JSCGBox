dd8a2f058a3ed384ab8e40bcdbb7806dfcc7a0d7
Sam.Tek <sonny.foot@hotmail.com>
2021-06-16 14:38:20 +0200

    Add ProposalBusiness

---
+++ Business/ProposalBusiness.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Repositories.Contracts;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProposalBusiness
+    {
+        private IProposalRepository _proposalRepository;
+        
+        public ProposalBusiness(IProposalRepository proposalRepository)
+        {
+            _proposalRepository = proposalRepository;
+        }
+
+        public async Task<IQueryable<Proposal>> GetProposalsAsync()
+        {
+            return await _proposalRepository.GetProposalsAsync();
+        }
+
+        public async Task<Proposal> DetailAsync(int id)
+        {
+            return await _proposalRepository.DetailAsync(id);
+        }
+
+        public async Task CreateAsync(Proposal proposal)
+        {
+            await _proposalRepository.CreateAsync(proposal);
+        }
+
+        public async Task EditAsync(Proposal proposal)
+        {
+            await _proposalRepository.EditAsync(proposal);
+        }
+
+        public async Task DeleteAsync(Proposal proposal)
+        {
+            await _proposalRepository.DeleteAsync(proposal);
+        }
+    }
+}
