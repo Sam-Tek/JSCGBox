ab8ba33af503958b3a094c71479b7991ba240bc5
Gaétan Sorin <sorin_gaetan@yahoo.fr>
2021-06-16 14:35:45 +0200

    Création du QuestionnaireBusiness et son interface

---
+++ Business.Contracts/IQuestionnaireBusiness.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contracts
+{
+    public interface IQuestionnaireBusiness
+    {
+        public Task<IQueryable<Questionnaire>> GetQuestionnairesAsync();
+
+        public Task<Questionnaire> DetailAsync(int id);
+
+        public Task CreateAsync(Questionnaire result);
+
+        public Task EditAsync(Questionnaire result);
+
+        public Task DeleteAsync(Questionnaire result);
+    }
+}
