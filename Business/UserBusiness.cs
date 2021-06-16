622f51d89bca0a7f358f5a215a45ff861909cb51
Sam.Tek <sonny.foot@hotmail.com>
2021-06-16 14:26:51 +0200

    Add UserBusiness

---
+++ Business/UserBusiness.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Repositories.Contracts;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class UserBusiness
+    {
+        private IUserRepository _userRepository;
+        
+        public UserBusiness(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IQueryable<User>> GetUsersAsync()
+        {
+            return await _userRepository.GetUsersAsync();
+        }
+
+        public async Task<User> DetailAsync(int id)
+        {
+            return await _userRepository.DetailAsync(id);
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            await _userRepository.CreateAsync(user);
+        }
+
+        public async Task EditAsync(User user)
+        {
+            await _userRepository.EditAsync(user);
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            await _userRepository.DeleteAsync(user);
+        }
+    }
+}
