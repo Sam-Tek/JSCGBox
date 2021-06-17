2595f50fc1f2ba12b3f65281be2cd7fc79c2330f
Jérémy Saudemont <jeremysaudemont@gmail.com>
2021-06-17 11:58:11 +0200

    Ajout controller de base

---
+++ WebApp/Controllers/Admin/QuestionaireController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.Admin
+{
+    public class QuestionaireController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Controllers/QuestionnaireController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            // ReSharper disable once Mvc.ViewNotResolved
+            return View();
+        }
+    }
+}
