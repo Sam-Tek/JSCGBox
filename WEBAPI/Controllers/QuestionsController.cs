bff2543ebe18938f014baad88d722a4d0eafdd6b
Claude-Bright git <claude4518@gmail..com>
2021-06-21 16:50:06 +0200

    Projet WEBAPI

---
+++ WEBAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,39 @@
+﻿using Business.Contracts;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private IQuestionBusiness _business;
+       
+
+        public QuestionsController(IQuestionBusiness business)
+        {
+    
+            _business = business;
+        }
+
+        [HttpGet]
+        public async Task<IQueryable<Question>> GetQuestionsAsync()
+        {
+            return await _business.GetQuestionsAsync();
+        }
+
+
+        //[HttpGet("{id}")]
+       // public async Task<ActionResult<Question>> GetQuestionsByQuestionnaireAsync(int id)
+        //{
+        //    return await _business.GetQuestionsByQuestionnaireAsync(id);
+       // }
+
+    }
+}
