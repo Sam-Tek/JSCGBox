5d520a18ec0e3307b57762d1be93d51891f89dc4
Sam.Tek <sonny.foot@hotmail.com>
2021-06-30 14:09:09 +0200

    Add code form gaetan to check the repository, and translate in french

--- BusinessTests/QuestionnaireBusinessTests.cs
+++ BusinessTests/QuestionnaireBusinessTests.cs
@@ -24,5 +24,28 @@ namespace Business.Tests
             // assert
             Assert.AreEqual(questionnaires.Count(), 1);
         }
+
+        //Code Gaëtan pour tester le repositorie
+        //[TestMethod()]
+        //public void GetLastPostTest()
+        //{
+        //    // Créer une BDD en mémoire dédié au test
+        //    // Nécéssite le package Microsoft.EntityFrameworkCore.InMemory
+        //    var builder = new DbContextOptionsBuilder<WikyCoreDbContext>()
+        //                     .UseInMemoryDatabase("Wiky");
+
+        //    var context = new WikyCoreDbContext(null, builder.Options);
+
+        //    context.Database.EnsureDeleted(); // Supprime la BDD entre chaque test
+        //                                      // Act + Assert
+        //    context.Posts.Add(new Entities.Post { Id = 1, Author = "a", DateCreate = DateTime.Now.AddHours(-1) });
+        //    context.Posts.Add(new Entities.Post { Id = 2, Author = "a", DateCreate = DateTime.Now });
+        //    context.SaveChanges();
+        //    var repo = new PostRepository(context);
+
+        //    var last = repo.GetLastPost();
+
+        //    Assert.AreEqual(last.Id, 2);
+        //}
     }
 }
